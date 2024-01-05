29fb974d2672eaee168856356c780e81da2211b4
Max Rydahl Andersen <max.andersen@gmail.com>
2024-01-05 22:27:10 +0100

    version provider need to be unremovable

--- src/main/java/dk/xam/hassq/commands/VersionProvider.java
+++ src/main/java/dk/xam/hassq/commands/VersionProvider.java
@@ -2,6 +2,7 @@ package dk.xam.hassq.commands;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.quarkus.arc.Unremovable;
 import jakarta.inject.Singleton;
 import picocli.CommandLine.IVersionProvider;
 
@@ -9,9 +10,12 @@ import picocli.CommandLine.IVersionProvider;
  * This class provides the version of the application.
  * It implements the IVersionProvider interface from the picocli library.
  * The @Singleton annotation enables us to inject this into picocli globally inside Quarkus.
+ * The @Unremovable annotation is needed to prevent Quarkus from removing this class during build time
+ * as Picocli will not be able to find it during runtime.
+ * 
  * The version is injected from the "quarkus.application.version" configuration property.
  **/
-@Singleton
+@Unremovable @Singleton
 public class VersionProvider implements IVersionProvider {
 
     @ConfigProperty(name = "quarkus.application.version")
